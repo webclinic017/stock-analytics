91f42323480ea39d8bff0cb440ccb4c385cc4b40
elt12opa <elt12opa@student.lu.se>
2021-04-11 19:02:57 +0200

    adding stuff in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pandas as pd
+
 from policies.baselines import RoundRobin
 from env.bitcoin_simple_env import BitcoinTradingEnv
+from actionMaker import *
 
 data_path = "data/kaggle_bitcoin_preprocessed.pkl"
 env = BitcoinTradingEnv(data_path)
@@ -10,7 +11,13 @@ policy = RoundRobin()
 
 while True:
     #print("RUN")
-    action = policy(obs)
+    smarthandler = handle_data(obs)
+
+    if smarthandler != 2:
+        action = smarthandler
+    else:
+        action = policy(obs)
+
     obs, reward, done, d = env.step(action)
     print(d["steps_left"], done, d["net_worth"])
     if done:
