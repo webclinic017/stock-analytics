770cccd20f535ee3bc16d151343d6baee4b65d41
elt12opa <elt12opa@student.lu.se>
2021-04-18 20:52:03 +0200

    Adding scalping feature with EMA capabilities

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from policies.Scalping import scalpingAgent
 from policies.rsi import BasicRSIAgent
 from env.bitcoin_simple_env import BitcoinTradingEnv
 from tqdm import tqdm
@@ -6,25 +7,30 @@ from datetime import datetime
 
 df = pd.read_csv("./data/kaggle_bitcoin.csv")
 df["Date"] = pd.to_datetime(df["Timestamp"], unit="s")
-start_date = datetime(year=2019, month=2, day=1)
-steps = 2000
+start_date = datetime(year=2015, month=2, day=1)
+steps = 45000
 
 
 env = BitcoinTradingEnv(df, start_date, steps=steps)
 obs = env.reset()
 policy = BasicRSIAgent()
+policy2 = scalpingAgent()
 n_steps = len(env.df)
 
 for i in tqdm(range(n_steps)):
-    action = policy(obs)
+    #action = policy(obs)
+    action = policy2(obs)
     obs, reward, done, d = env.step(action)
     if done:
         break
 
 df_res = obs.dropna()
-max_gain = df_res['Close'].iloc[0] / df_res['Close'].iloc[-1] * 100
-gain = (df_res['net_worth'].iloc[0]/df_res['net_worth'].iloc[-1]) * 100
-print("% Capital from start: ", gain)
-print("% Bitcoin price: ", max_gain)
+max_gain = df_res['Close'].iloc[-1]/df_res['Close'].iloc[0] * 100
+gain = df_res['net_worth'].iloc[-1]/ df_res['net_worth'].iloc[0] * 100
+print("% Capital from start: ", df_res['net_worth'].iloc[0])
+print("% Capital at end: ", df_res['net_worth'].iloc[-1])
+print("% Bitcoin trading yield in : ", gain)
+print("% Bitcoin no actions %: ", max_gain)
+
 
 policy.render(df_res)
